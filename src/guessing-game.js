b268501a162eaee037e28f2bb46e3c5cfdccb749
rebeljuls <rebel.90@mail.ru>
2023-02-26 17:16:48 +0100

    refactor: code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.currGuessingGame = 0;
+    }
 
-    setRange(min, max) {
-
+    setRange(_min, _max) {
+        this.min = _min;
+        this.max = _max;
     }
 
     guess() {
-
+        return this.currGuessingGame = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.currGuessingGame;
     }
 
     greater() {
-
+        this.min = this.currGuessingGame;
     }
 }
 
